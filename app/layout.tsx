66e41f97a0267eb0dbf2320164f871bf3c925eca
emmanuellegonidec-cmd <emmanuelle.gonidec@gmail.com>
2026-04-18 12:03:45 +0200

    Fix: Montserrat chargée globalement via <link> dans le layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,6 +42,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+  href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400&display=swap"
+  rel="stylesheet"
+/>
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-LKZHGMP0WG"
           strategy="afterInteractive"
